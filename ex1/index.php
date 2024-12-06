7321e1ac22e7d468310a0b857daa7e8615f007cc
SireneNouira <sirene.nouira16@gmail.com>
2024-12-06 16:42:05 +0100

    tp a faire

---
+++ ex1/index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+// $date =  date("l/d/F/Y H:i:s");
+
+// echo $date;
+
+// $date = time();
+
+// $stt = strtotime('2016/08/2 15:00:00');
+
+// echo $stt;
+
+
+ //ex5
+
+// $date = time();
+// $dateCible = strtotime('2016-05-16');
+// $diff = $date-$dateCible;
+
+// echo "<br>";
+
+// echo $diff;
+
+
+ //ex6
+
+//  $number = cal_days_in_month(CAL_GREGORIAN, 2, 2016);
+
+//  echo $number;
+
+ //ex7
+
+//  $date = time();
+
+ 
+//  echo date("d-m-Y", strtotime("+20 days"));
+
+//ex8
+
+ $date = time();
+
+ 
+ echo date("d-m-Y", strtotime("-20 days"));
